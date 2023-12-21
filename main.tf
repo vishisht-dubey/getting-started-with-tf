2333e282c87f17947c59def90ea021cfa4569cae
Vishisht Dubey <98794736+vishisht-dubey@users.noreply.github.com>
2023-12-21 19:42:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,31 @@
-# Terraform Block
-terraform {
-  required_version = ">= 0.15"
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      # version = "1.44.0"
-      version = ">= 2.0" 
-    }
-    random = {
-      source = "hashicorp/random"
-      version = ">= 3.0"
-    }
+# Resource-1: Azure Resource Group
+resource "azurerm_resource_group" "myrg1" {
+  name = "myrg-1.0"
+  location = "East US"
+}
+
+# Resource-2: Random String 
+resource "random_string" "myrandom" {
+  length = 16
+  upper = false 
+  special = false
+}
+
+# Resource-3: Azure Storage Account 
+resource "azurerm_storage_account" "mysa" {
+  name                     = "mysa${random_string.myrandom.id}"
+  resource_group_name      = azurerm_resource_group.myrg1.name
+  location                 = azurerm_resource_group.myrg1.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  # account_encryption_source = "Microsoft.Storage"
+
+  tags = {
+    environment = "staging"
   }
 }
-
-# Provider Block
-provider "azurerm" {
-features {}          
+resource "azurerm_storage_container" "mycontainer" {
+  name                  = "myfirstcontainer"
+  storage_account_name  = azurerm_storage_account.mysa.name
+  container_access_type = "private"
 }
-
-
