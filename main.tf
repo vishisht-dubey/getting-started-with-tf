00b1711603f234cccdb2ed81c26102988a545794
Vishisht Dubey <98794736+vishisht-dubey@users.noreply.github.com>
2023-12-21 22:48:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Resource-1: Azure Resource Group
-resource "azurerm_resource_group" "myrg1" {
-  name = "myrg-1.0"
+resource "azurerm_resource_group" "myrg2" {
+  name = "myrg-2.0"
   location = "East US"
 }
 
@@ -14,8 +14,8 @@ resource "random_string" "myrandom" {
 # Resource-3: Azure Storage Account 
 resource "azurerm_storage_account" "mysa" {
   name                     = "mysa${random_string.myrandom.id}"
-  resource_group_name      = azurerm_resource_group.myrg1.name
-  location                 = azurerm_resource_group.myrg1.location
+  resource_group_name      = azurerm_resource_group.myrg2.name
+  location                 = azurerm_resource_group.myrg2.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
   # account_encryption_source = "Microsoft.Storage"
